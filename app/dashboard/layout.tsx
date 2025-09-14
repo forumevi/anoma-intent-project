e888bb81a32b9dda1e00ab7a2289c1280e0cf4bb
forumevi <35641620+forumevi@users.noreply.github.com>
2025-09-14 23:18:53 +0300

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,11 +1,13 @@
-import Sidebar from '../../components/Sidebar';
+"use client";
+
+import Sidebar from "../../components/Sidebar";
 import '../globals.css';
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex">
+    <div className="flex min-h-screen">
       <Sidebar />
-      <main className="flex-1 p-6 bg-gray-900 text-white min-h-screen">
+      <main className="flex-1 p-6 bg-bgDark">
         {children}
       </main>
     </div>
